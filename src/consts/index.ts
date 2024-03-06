9f728f0b12a94841543f039262ef9a684f7db1a1
Takuyawe <takuslugger200@gmail.com>
2024-03-06 18:16:38 +0900

    feature: cssのスタイリング適用

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,4 +1,4 @@
-import { WEEK_DAY_TYPE } from "../types";
+import { WEATHER_TYPE, WEEK_DAY_TYPE } from "../types";
 
 export const WEEK_DAY: Record<number, WEEK_DAY_TYPE> = {
     0: 'Sun',
@@ -9,3 +9,10 @@ export const WEEK_DAY: Record<number, WEEK_DAY_TYPE> = {
     5: 'Fri',
     6: 'Sat',
 };
+
+export const WEATHER: WEATHER_TYPE = {
+    sunny: '晴れ',
+    Clouds: '曇り',
+    rain: '雨',
+    snowy: '雪',
+}
