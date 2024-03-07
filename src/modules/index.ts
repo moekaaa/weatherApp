597ab234e97ecc2f986aa619c2b4e3e6071bac49
Takuyawe <takuslugger200@gmail.com>
2024-03-07 12:42:07 +0900

    refactor: add template literal

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -7,5 +7,6 @@ export function getTodayDate (): string {
     const day = date.getDate();
     const weekDayNum = date.getDay(); // 曜日を数値で取得する
     const weekDayName = (WEEK_DAY[weekDayNum]); // 曜日を文字列に変換
-    return month + "月" + day + "日 " + weekDayName;
+    const dateString = `${month}月${day}日(${weekDayName})`; // テンプレート文字列を使って日付を作成
+    return dateString
 }
