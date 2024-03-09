208bedc8ddd71cbb63133541b74b99a28b6b2f98
Takuyawe <takuslugger200@gmail.com>
2024-03-09 17:08:17 +0900

    feature: add type undefined to api response

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,18 +1,19 @@
 import { WEATHER_TYPE, WEEK_DAY_TYPE } from "../types";
 
 export const WEEK_DAY: Record<number, WEEK_DAY_TYPE> = {
-    0: 'Sun',
-    1: 'Mon',
-    2: 'Tue',
-    3: 'Wed',
-    4: 'Thu',
-    5: 'Fri',
-    6: 'Sat',
+  0: "Sun",
+  1: "Mon",
+  2: "Tue",
+  3: "Wed",
+  4: "Thu",
+  5: "Fri",
+  6: "Sat",
 };
 
 export const WEATHER: WEATHER_TYPE = {
-    Sunny: '晴れ',
-    Clouds: '曇り',
-    Rain: '雨',
-    Snowy: '雪',
-}
+  Sunny: "晴れ",
+  Clouds: "曇り",
+  Rain: "雨",
+  Snowy: "雪",
+  undefined: "不明",
+};
