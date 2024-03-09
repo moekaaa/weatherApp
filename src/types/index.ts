675a29a17fb49b9a4f15a48213f81fec6751a340
Takuyawe <takuslugger200@gmail.com>
2024-03-09 17:08:17 +0900

    feature: add type undefined to api response

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,17 @@
 // ユニオン型というタイプ定義
-export type WEEK_DAY_TYPE = 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat';
+export type WEEK_DAY_TYPE =
+  | "Sun"
+  | "Mon"
+  | "Tue"
+  | "Wed"
+  | "Thu"
+  | "Fri"
+  | "Sat";
 
-// TODO_Moeka: 値がundefinedの場合の値を追加する
-export type WEATHER_EN = "Sunny" | "Clouds" | "Rain" | "Snowy";
+export type WEATHER_EN = "Sunny" | "Clouds" | "Rain" | "Snowy" | "undefined";
 
-type WEATHER_JP = "晴れ" | "曇り" | "雨" | "雪";
+type WEATHER_JP = "晴れ" | "曇り" | "雨" | "雪" | "不明";
 
 // Record型というタイプ定義
 // {key: value}というオブジェクトの定義に使われる
 export type WEATHER_TYPE = Record<WEATHER_EN, WEATHER_JP>;
-
