9015e1a2b50c836f8c63512967f9b1047d3385c7
Takuyawe <takuslugger200@gmail.com>
2024-03-10 22:49:40 +0900

    feature: add Todo

--- src/components/WeatherDetail.vue
+++ src/components/WeatherDetail.vue
@@ -1,79 +1,82 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import {getTodayDate} from "../modules"
-import {WEATHER} from "../consts"
-import { WEATHER_EN } from '../types';
+import { ref } from "vue";
+import { getTodayDate } from "../modules";
+import { WEATHER } from "../consts";
+import { WEATHER_EN } from "../types";
 
 // TODO_Moeka: APIキーを.envファイルに移動
 const apiKey = "37639b3722b929b763122aeed5ee06eb";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-const city = ref("")
+const city = ref("");
 
 const weatherDetail = ref({
-  weather: '',
-  temp_max: '',
-  temp_min: '',
-  wind: '',
-  humidity: '',
-})
+  weather: "",
+  temp_max: "",
+  temp_min: "",
+  wind: "",
+  humidity: "",
+});
 
-const isDataFetched = ref(false)
+const isDataFetched = ref(false);
 
-const today = ref(getTodayDate())
-
+const today = ref(getTodayDate());
 
 // TODO_Moeka: この関数もmodules/に移動
 async function fetchWeather(): Promise<void> {
-    // TODO_Moeka: urlとapiKeyを.envファイルからロード(dotenvライブラリ)
-  const response = await fetch(apiUrl + city.value + "&appid=" + apiKey + "&units=metric")
+  // TODO_Moeka: urlとapiKeyを.envファイルからロード(dotenvライブラリ)
+  const response = await fetch(
+    apiUrl + city.value + "&appid=" + apiKey + "&units=metric"
+  );
 
   // TODO_Moeka: 天気の情報が返ってこなかった時(存在しない都市の名前の入力時)の処理を追加(エラーハンドリング)
   // try catch文を使用する
 
-  const data = await response.json()
+  const data = await response.json();
 
-  if (!data) return
+  if (!data) return;
 
-  weatherDetail.value.weather = "天気: " + WEATHER[data.weather[0].main as WEATHER_EN];
+  weatherDetail.value.weather =
+    "天気: " + WEATHER[data.weather[0].main as WEATHER_EN];
   weatherDetail.value.temp_max = Math.round(data.main.temp_max) + "°C";
   weatherDetail.value.temp_min = Math.round(data.main.temp_min) + "°C";
   weatherDetail.value.wind = "風速: " + data.wind.speed + "km/h";
   weatherDetail.value.humidity = "湿度: " + data.main.humidity + "%";
 
-  isDataFetched.value = true 
+  isDataFetched.value = true;
 }
 </script>
 
 <template>
   <div class="showWeather">
-      <div class="search">
-        <input type="text" v-model="city" placeholder="city name"/>
-        <button @click="fetchWeather">Get Weather</button>
-      </div>
+    <div class="search">
+      <input type="text" v-model="city" placeholder="city name" />
+      <button @click="fetchWeather">Get Weather</button>
+    </div>
 
-      <p class="today"></p>
-      <p id="weekDayName"></p>
+    <p class="today"></p>
+    <p id="weekDayName"></p>
 
-      <div v-if="isDataFetched" class="weatherDetail">
-        <p class="today">今日の日付: {{ today }}</p>
-        <p class="weather">{{ weatherDetail.weather }}</p>
-        <div class="temp">
-            <p class="temp_min">{{ weatherDetail.temp_min }}</p>
-            <div class="temp_border"></div>
-            <p class="temp_max">{{ weatherDetail.temp_max }}</p>
-        </div>
-        <p class="wind">{{ weatherDetail.wind }}</p>
-        <p class="humidity">{{ weatherDetail.humidity }}</p>
+    <div v-if="isDataFetched" class="weatherDetail">
+      <p class="today">今日の日付: {{ today }}</p>
+      <!-- TODO: 天気によってアイコンを表示 -->
+      <p class="weather">{{ weatherDetail.weather }}</p>
+      <div class="temp">
+        <p class="temp_min">{{ weatherDetail.temp_min }}</p>
+        <div class="temp_border"></div>
+        <p class="temp_max">{{ weatherDetail.temp_max }}</p>
       </div>
+      <p class="wind">{{ weatherDetail.wind }}</p>
+      <p class="humidity">{{ weatherDetail.humidity }}</p>
+    </div>
   </div>
 </template>
 
 <style>
-.showWeather{
-    height: 300px;
-    width: 100%;
-    background-color: gray;
+.showWeather {
+  height: 300px;
+  width: 100%;
+  background-color: gray;
 }
 .search {
   display: flex;
@@ -91,18 +94,18 @@ async function fetchWeather(): Promise<void> {
   margin-top: 1rem;
 }
 .temp {
-    display: flex;
+  display: flex;
 }
 .temp_min {
-    color: blue;
+  color: blue;
 }
 .temp_border {
-    width: 1px;
-    height: 28px;
-    background-color: white;
-    margin: 0 10px;
+  width: 1px;
+  height: 28px;
+  background-color: white;
+  margin: 0 10px;
 }
 .temp_max {
-    color: red;
+  color: red;
 }
 </style>
