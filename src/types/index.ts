773f50c8ab404779d8691175772158cd5b7dab2c
Takuyawe <takuslugger200@gmail.com>
2024-03-13 15:13:23 +0900

    feature: add a different way for type definition

--- src/types/index.ts
+++ src/types/index.ts
@@ -15,3 +15,8 @@ type WEATHER_JP = "晴れ" | "曇り" | "雨" | "雪" | "不明";
 // Record型というタイプ定義
 // {key: value}というオブジェクトの定義に使われる
 export type WEATHER_TYPE = Record<WEATHER_EN, WEATHER_JP>;
+
+// Mapped Typesを使った書き方
+// export type WEATHER_TYPE = {
+//   [key in WEATHER_EN]: WEATHER_JP
+// }
