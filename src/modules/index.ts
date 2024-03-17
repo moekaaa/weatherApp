0c04fc461262f47743a8ff2770f0da0aa490068c
moekaaa <ramenlady22@gmail.com>
2024-03-18 08:18:30 +0900

    mess

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,5 +1,30 @@
 import {WEEK_DAY} from "../consts"
 
+
+
+export async function fetchWeather (): Promise<void> {
+  // TODO_Moeka: urlとapiKeyを.envファイルからロード(dotenvライブラリ)
+  const response = await fetch(
+    import.meta.env.WEATHER_API_KEY + city.value + "&appid=" + import.meta.env.WEATHER_API_KEY + "&units=metric"
+  );
+
+  // TODO_Moeka: 天気の情報が返ってこなかった時(存在しない都市の名前の入力時)の処理を追加(エラーハンドリング)
+  // try catch文を使用する
+
+  const data = await response.json();
+
+  if (!data) return;
+
+  weatherDetail.value.weather =
+    "天気: " + WEATHER[data.weather[0].main as WEATHER_EN];
+  weatherDetail.value.temp_max = Math.round(data.main.temp_max) + "°C";
+  weatherDetail.value.temp_min = Math.round(data.main.temp_min) + "°C";
+  weatherDetail.value.wind = "風速: " + data.wind.speed + "km/h";
+  weatherDetail.value.humidity = "湿度: " + data.main.humidity + "%";
+
+  isDataFetched.value = true;
+}
+
 export function getTodayDate (): string {
     // TODO_Moeka: Tempoという日付管理ライブラリが便利そう、、
     const date = new Date();
