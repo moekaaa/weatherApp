609abc923b477d698ba4804e678003b204257728
Takuyawe <takuslugger200@gmail.com>
2024-03-19 14:29:48 +0900

    review: stores/index.tsをレビュー

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,29 +1,29 @@
-import { ref, reactive } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, reactive } from 'vue';
+import { defineStore } from 'pinia';
 
 //基本的に変数はここで管理するようにする
 //イベント発生ごとにstoreで状態管理するようにしてvueファイルでも
 //tsファイルでもstoreをimportして変数を使えるようにする
 
-
+// Review: piniaでの値の宣言方法が違うかも
 export const useUserStore = defineStore('user', () => {
-  const fname = ref('')
-  const lname = ref('')
+  const fname = ref('');
+  const lname = ref('');
   const today = ref('');
   const weatherDetail = ref({
     weather: '',
     temp_max: '',
     temp_min: '',
     wind: '',
-    humidity: ''
-  })
-  const city = ref('')
+    humidity: '',
+  });
+  const city = ref('');
 
-  return { 
-    fname, 
-    lname, 
+  return {
+    fname,
+    lname,
     today,
-    weatherDetail, 
-    city 
-  }
-})
+    weatherDetail,
+    city,
+  };
+});
