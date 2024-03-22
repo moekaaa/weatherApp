734ea6069f74c8aa8967a03d445db70d5b13c10f
Takuyawe <takuslugger200@gmail.com>
2024-03-22 15:16:59 +0900

    refactor: the constant variable TIPS_FOR_TODAY can be applied to the both for odd and even months

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,47 +12,12 @@ export type WEATHER_EN = "Sunny" | "Clouds" | "Rain" | "Snowy" | "undefined";
 
 type WEATHER_JP = "晴れ" | "曇り" | "雨" | "雪" | "不明";
 
-export type TIPS_FOR_TODAY = 
-1 | 
-2 | 
-3 | 
-4 | 
-5 | 
-6 | 
-7 | 
-8 | 
-9 | 
-10 | 
-11 | 
-12 | 
-13 | 
-14 | 
-15 | 
-16 | 
-17 | 
-18 | 
-19 | 
-20 | 
-21 | 
-22 | 
-23 | 
-24 | 
-25 | 
-26 | 
-27 | 
-28 | 
-29 | 
-30 | 
-31 ; 
+export type TIPS_FOR_TODAY = Record<number, string>;
 
 
 // Record型というタイプ定義
 // {key: value}というオブジェクトの定義に使われる
 export type WEATHER_TYPE = Record<WEATHER_EN, WEATHER_JP>;
-
-export type TIPS_FOR_TODAY_ODD = Record<number, string>;
-
-export type TIPS_FOR_TODAY_EVEN = Record<number, string>;
 
 // Mapped Typesを使った書き方
 // export type WEATHER_TYPE = {
