af4aa7aadb68054cc0191368bf86f5b8b1253cd1
Takuyawe <takuslugger200@gmail.com>
2024-03-25 16:11:05 +0900

    feat: add function to display message

--- src/components/userpanel.vue
+++ src/components/userpanel.vue
@@ -1,3 +1,16 @@
 //userに紐づく情報のパネル
 <script setup>
+import { onMounted } from 'vue';
+import { useUserDataStore } from '../stores';
+import { getCheerUpMessage, getTodayDate } from '../modules';
+
+const userDataStore = useUserDataStore()
+const cheerUpMessage = getCheerUpMessage()
+
 </script>
+
+<template>
+    <div class="user-panel">
+        <p>{{cheerUpMessage}}</p>
+    </div>
+</template>
