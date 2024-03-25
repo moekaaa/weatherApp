a14f5aa582e03a0eb03fcc6185648171ca0346b2
moekaaa <ramenlady22@gmail.com>
2024-03-26 08:37:58 +0900

    kaddStyle: add Styling and RouterFunction(not yet)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
-<script setup>
+<script setup lang="ts">
+import { reactive, provide } from "vue";
 import HomeView from "./views/HomeView.vue";
+//import { RouterView } from "vue-router";
+//import { MEMBER } from "/types";
+
+//いったん今はDBつながないで表示したい
+// const memberList = new Map<number, MEMBER>();
+// memberList.set(1, {
+//   id: 1,
+//   name: "Moeka",
+//   note: "student"
+// });
+// memberList.set(2, {
+//   id: 2,
+//   name: "Takuya",
+//   note: "teacher"
+// });
+// provide("memberList", reactive(memberList));
 </script>
 
 <template>
-  <HomeView />
+  <header>
+    <h1>vue practice</h1>
+  </header>
+  <main>
+    <!-- routerを使ってトップ画面表示させたい、、 -->
+      <RouterView />
+     <HomeView />
+  </main>
+  
 </template>
 
 <style>
+main {
+  border: white 1px solid;
+  padding: 10px;
+}
 *{
     margin: 0;
     padding: 0;
@@ -16,5 +45,24 @@ import HomeView from "./views/HomeView.vue";
 body{
   background: #555;
   color: white;
+}
+#breadcrumbs ul li {
+  display: inline;
+  list-style-type: none;
+}
+#breadcrumbs {
+  margin-left: 0px;
+}
+#breadcrumbs ul {
+  padding-left: 0px;
+}
+#breadcrumbs ul .current {
+  color: white;
+}
+#breadcrumbs ul li:before {
+  content: " > ";
+}
+#breadcrumbs ul li:first-child:before {
+  content: none;
 }
 </style>
