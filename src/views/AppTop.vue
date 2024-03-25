34f704f4e64d14315da880574b6f35281b724eb4
moekaaa <ramenlady22@gmail.com>
2024-03-26 08:42:29 +0900

    adddFunction: router

---
+++ src/views/AppTop.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HomeView from './HomeView.vue';
+
+</script>
+
+<template>
+    <h1>
+        AppTop
+    </h1>
+    <nav id = "breadcrumbs">
+        <ul>
+            <li>TOP</li>
+        </ul>
+    </nav>
+    <section>
+        <p>member管理はこちらから</p>
+         <HomeView/>
+        
+    </section>
+</template>
