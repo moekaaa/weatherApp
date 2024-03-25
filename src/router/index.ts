dbb4cabf36b4aa53d22f3ca2b10b31ee8b6ce374
moekaaa <ramenlady22@gmail.com>
2024-03-26 08:35:34 +0900

    addFunction: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,32 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+import AppTop from '../views/AppTop.vue'
+
 import HomeView from '../views/HomeView.vue'
 
 
-//const app = creteApp(App)
-//app.use(router)
-//app/MouseEvent('#app')
+const routeSettings: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'AppTop',
+    component: AppTop
+  },  
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes: routeSettings,
 
-// エラーが出るのでコメントアウト
-// export const getRoutes = () => {
-//   const routes: Array<RouteRecordRaw> = [
-// {
-//   path: '/weatherDetail',
-//   name: RouteName.WeatherDetail,
-//   component: WeatherDetail,
+});
+export default router;
 
-// }
-   
-//   ]
-// }
-
-export default router
+//ルーティング登録方法
+// const routeSettings: RouteRecordRaw[] = [
+//   {
+//     path: "パス文字列",
+//     name: "ルーティング名"
+//     component: 表示させる画面用コンポーネント
+//   },
+//   :
+//   :
+// ];
