0c468d47418f26c81697fbbf3029347d22fb75ed
moekaaa <ramenlady22@gmail.com>
2024-03-27 08:24:20 +0900

    feature: add register license of syncfusion library?

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
+import { registerLicense } from "@syncfusion/ej2-base";
 
 import './style.css'
+
+registerLicense("");
 
 const app = createApp(App)
 app.use(router)
