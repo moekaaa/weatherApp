3fae75855b1666be98dba922867a1722d83265bc
Takuyawe <takuslugger200@gmail.com>
2024-03-26 16:39:41 +0900

    refactor: remove unused code

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,55 +1,51 @@
-import { CHEERUP_FOR_EVEN_NUMBER_MONTH, CHEERUP_FOR_ODD_NUMBER_MONTH, WEEK_DAY } from '../consts';
+import {
+  CHEERUP_FOR_EVEN_NUMBER_MONTH,
+  CHEERUP_FOR_ODD_NUMBER_MONTH,
+  WEEK_DAY,
+} from '../consts';
 // Review: ファイルとフォルダの名前を合わせる
 import { useWeatherDataStore } from '../stores';
 
- import { WEATHER } from '../consts';
- import { WEATHER_EN } from '../types';
-
-
-
-// Review: useUserStoreという名前に変更(../stores/index.tsのファイルではこの名前で定義されている)
+import { WEATHER } from '../consts';
+import { WEATHER_EN } from '../types';
 
 const apiKey = import.meta.env.VITE_API_KEY;
 const apiUrl = import.meta.env.VITE_API_URL;
 
-// Review: https://zenn.dev/sa2knight/books/storybook-7-with-vue-3/viewer/pinia
-// Review: このサイトがわかりやすそう→getterとsetterしなくてもよい書き方だとSecurityてきに問題ありますか？
-//これが使えそう？　→　https://pinia.vuejs.org/introduction.html
-
 export const getWeatherData = async () => {
-// useWeatherData を使って天気情報を更新する
+  // useWeatherData を使って天気情報を更新する
   //useWeatherData.today = new Date().toLocaleDateString();
- // useWeatherData.city = useWeatherData.CITY[useWeatherData.cityIndex]; 
-    const weatherDataStore = useWeatherDataStore();
+  // useWeatherData.city = useWeatherData.CITY[useWeatherData.cityIndex];
+  const weatherDataStore = useWeatherDataStore();
 
-    const url = apiUrl + weatherDataStore.city + '&appid=' + apiKey + '&units=metric'  
+  const url =
+    apiUrl + weatherDataStore.city + '&appid=' + apiKey + '&units=metric';
 
-   // fetch を使って天気情報を取得する
-    const response = await fetch(url);
-    try {
-      const data = await response.json();
+  // fetch を使って天気情報を取得する
+  const response = await fetch(url);
+  try {
+    const data = await response.json();
 
-      console.log(data)
- 
-      // useWeatherData を使って天気情報を更新する
-      weatherDataStore.weatherDetail.weather =
-        '天気: ' + WEATHER[data.weather[0].main as WEATHER_EN];
-      weatherDataStore.weatherDetail.tempMax = Math.round(data.main.temp_max) + '°C';
-      weatherDataStore.weatherDetail.tempMin = Math.round(data.main.temp_min) + '°C';
-      weatherDataStore.weatherDetail.wind = '風速: ' + data.wind.speed + 'km/h';
-      weatherDataStore.weatherDetail.humidity = '湿度: ' + data.main.humidity + '%';
+    // useWeatherData を使って天気情報を更新する
+    weatherDataStore.weatherDetail.weather =
+      '天気: ' + WEATHER[data.weather[0].main as WEATHER_EN];
+    weatherDataStore.weatherDetail.tempMax =
+      Math.round(data.main.temp_max) + '°C';
+    weatherDataStore.weatherDetail.tempMin =
+      Math.round(data.main.temp_min) + '°C';
+    weatherDataStore.weatherDetail.wind = '風速: ' + data.wind.speed + 'km/h';
+    weatherDataStore.weatherDetail.humidity =
+      '湿度: ' + data.main.humidity + '%';
 
- //     useWeatherData.isDataFetched.value = true;
-    } catch (error: unknown) {
-      // errorをanyかunknown型にする違いを調べてみてください
-      // if(something instanceof type)という書き方は、タイプガードと言って、errorはError型の時のみ実行する
-      if (error instanceof Error) throw new Error("Error: " + error.message);
-    }  
- }
-
+    //     useWeatherData.isDataFetched.value = true;
+  } catch (error: unknown) {
+    // errorをanyかunknown型にする違いを調べてみてください
+    // if(something instanceof type)という書き方は、タイプガードと言って、errorはError型の時のみ実行する
+    if (error instanceof Error) throw new Error('Error: ' + error.message);
+  }
+};
 
 export function getTodayDate(): string {
-  // Tempoという日付管理ライブラリが便利そう、、
   const date = new Date();
   const month = date.getMonth() + 1;
   const day = date.getDate();
@@ -60,12 +56,12 @@ export function getTodayDate(): string {
 }
 
 export const getCheerUpMessage = () => {
-  const newDate = new Date()
-  const month = newDate.getMonth() + 1
-  const date = newDate.getDate()
+  const newDate = new Date();
+  const month = newDate.getMonth() + 1;
+  const date = newDate.getDate();
   if (month % 2 === 0) {
-    return CHEERUP_FOR_EVEN_NUMBER_MONTH[date]
+    return CHEERUP_FOR_EVEN_NUMBER_MONTH[date];
   } else {
-    return CHEERUP_FOR_ODD_NUMBER_MONTH[date]
+    return CHEERUP_FOR_ODD_NUMBER_MONTH[date];
   }
-} 
+};
