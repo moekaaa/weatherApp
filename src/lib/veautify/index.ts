b701d6c309e9c43e5f4ef111342fdba52d78b217
Takuyawe <takuslugger200@gmail.com>
2024-03-27 11:03:09 +0900

    feat: add a UI component library veautify

---
+++ src/lib/veautify/index.ts
@@ -0,0 +1,11 @@
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const veautify = createVuetify({
+  components,
+  directives,
+});
+
+export default veautify;
