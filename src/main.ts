c62fe156b01c108b40bb00a98f36edad5080d7bd
Takuyawe <takuslugger200@gmail.com>
2024-03-27 11:03:09 +0900

    feat: add a UI component library veautify

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import  Vue from 'vue';
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-import router from './router'
-import { registerLicense } from "@syncfusion/ej2-base";
+import Vue from 'vue';
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createPinia } from 'pinia';
+import router from './router';
+import { registerLicense } from '@syncfusion/ej2-base';
+import veautify from './lib/veautify';
 
-import './style.css'
+import './style.css';
 
-registerLicense("");
+registerLicense('');
 
-const app = createApp(App)
-app.use(router)
-app.use(createPinia())
+const app = createApp(App);
+app.use(router);
+app.use(veautify);
+app.use(createPinia());
 
-app.mount('#app')
+app.mount('#app');
