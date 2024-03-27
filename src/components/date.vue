63744d8a87766512e5e3c4a720b804f71e39219d
moekaaa <ramenlady22@gmail.com>
2024-03-28 07:28:28 +0900

    feat: add date

--- src/components/date.vue
+++ src/components/date.vue
@@ -6,7 +6,13 @@ const name =ref("moeka");
 //ref, setInterval, functionの使い方理解
 
 const now = new Date();
-//現在の時刻の文字列を取得
+
+
+const date = ref(now.getDate());
+const month = ref(now.getMonth() + 1);
+const year = ref(now.getFullYear());
+const today = ref(`${year.value}/${month.value}/${date.value}`);
+
 const nowStr = now.toLocaleTimeString();
 //同じく現在の時刻文字列をテンプレート変数としてrefで用意
 const timeStrRef = ref(nowStr);
@@ -14,12 +20,9 @@ const timeStrRef = ref(nowStr);
 
 //新しい時刻に変更する関数
 function changeTime(): void {
-  //現在日時を取得
+
   const newTime = new Date();
-  //現在の時刻の文字列を取得
   const newTimeStr = newTime.toLocaleTimeString();
-  //現在の時刻文字列をテンプレート変数timeStrRefに格納
-  //valueないとダメ（valueプロパティはref()によって生成された変数をリアクティブな状態を維持してくれる？）
   timeStrRef.value = newTimeStr;
 
 }
@@ -32,7 +35,8 @@ setInterval(changeTime, 1000)
 
 <template>
   <h1>こんにちは！{{name}} さん</h1>
-  <p>現在時刻：{{ timeStrRef }}</p>
+  <h1>{{ today }} </h1>
+  <p>{{ timeStrRef }}</p>
   <WeatherDetail />
 </template>
 
