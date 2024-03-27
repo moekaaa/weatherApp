89aaf9908b2dca12ce47dd1cd44e9cecc020ee78
Takuyawe <takuslugger200@gmail.com>
2024-03-27 11:26:15 +0900

    feat: add theme button

---
+++ src/components/menuBar/DarkModeButton.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <button class="light-dark-mode-button">
+    <v-icon color="white" icon="fa:fas fa-circle-half-stroke"></v-icon>
+  </button>
+</template>
+
+<style>
+.light-dark-mode-button {
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+}
+</style>
--- src/views/AppTop.vue
+++ src/views/AppTop.vue
@@ -1,20 +1,18 @@
 <script setup>
 import HomeView from './HomeView.vue';
-
+import MenuBar from '../components/menuBar/MenuBar.vue';
 </script>
 
 <template>
-    <h1>
-        AppTop
-    </h1>
-    <nav id = "breadcrumbs">
-        <ul>
-            <li>TOP</li>
-        </ul>
-    </nav>
-    <section>
-        <p>member管理はこちらから</p>
-         <HomeView/>
-        
-    </section>
+  <h1>AppTop</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>TOP</li>
+    </ul>
+  </nav>
+  <MenuBar />
+  <section>
+    <p>member管理はこちらから</p>
+    <HomeView />
+  </section>
 </template>
