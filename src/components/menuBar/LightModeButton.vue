a1fbcb209f457cfcfe28f28ff13ba489a5417bbe
moekaaa <ramenlady22@gmail.com>
2024-03-28 19:23:10 +0900

    feat: add theme button

---
+++ src/components/menuBar/LightModeButton.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <button class="dark-light-dark-mode-button">
+    <v-icon color="white" icon="fa-solid fa-circle"></v-icon>
+  </button>
+</template>
+
+<style>
+.dark-light-mode-button {
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+}
+</style>
