7c9e43a18a912b03dc0d1c5d42ddcb72efd7be4c
moekaaa <ramenlady22@gmail.com>
2024-03-29 07:19:34 +0900

    refactor: rename the file and change function

---
+++ src/components/menuBar/LangModeButton.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <button class="dark-light-dark-mode-button">
+    <v-icon color="white" icon="fa-solid fa-earth-asia"></v-icon>
+  </button>
+</template>
+
+<style>
+.dark-light-mode-button {
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+}
+</style>
