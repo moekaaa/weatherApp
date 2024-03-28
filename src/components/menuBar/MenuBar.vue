e13fc1b29ad797e4b738e18a2d612fb0a8793469
moekaaa <ramenlady22@gmail.com>
2024-03-29 07:19:34 +0900

    refactor: rename the file and change function

--- src/components/menuBar/MenuBar.vue
+++ src/components/menuBar/MenuBar.vue
@@ -1,9 +1,20 @@
 <script setup>
 import DarkModeButton from './DarkModeButton.vue';
+import LightModeButton from './LangModeButton.vue';
 </script>
 
 <template>
-  <div>
+  <div class="menuButton">
     <DarkModeButton />
+    <LightModeButton />
   </div>
 </template>
+
+<style>
+.menuButton {
+  display: flex;
+  justify-content: space-between;
+  margin-left: 0 auto;
+  width: 120px;
+}
+</style>
