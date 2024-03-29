afa2b33dadceda0b74d16eff83cba815599876bf
Takuyawe <takuslugger200@gmail.com>
2024-03-29 19:06:27 +0900

    feat: theme color config

--- src/components/calendar/Schedule.vue
+++ src/components/calendar/Schedule.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup lang="ts">
 import { ScheduleComponent } from '@syncfusion/ej2-vue-schedule';
 //syncfusion/ej2-vue-schedule　↓ これで作ってみようと思っています
 //https://takumi-oda.com/blog/2020/06/24/post-1405/
