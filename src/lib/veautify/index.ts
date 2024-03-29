a871409c5cad6aadcd3821b2c9a009d874f01ba3
Takuyawe <takuslugger200@gmail.com>
2024-03-29 14:22:51 +0900

    feat: add veautify component calendar

--- src/lib/veautify/index.ts
+++ src/lib/veautify/index.ts
@@ -3,9 +3,13 @@ import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { aliases, fa } from 'vuetify/iconsets/fa';
+import { VCalendar } from 'vuetify/labs/VCalendar';
 
 const veautify = createVuetify({
-  components,
+  components: {
+    components,
+    VCalendar,
+  },
   directives,
   icons: {
     defaultSet: 'fa',
