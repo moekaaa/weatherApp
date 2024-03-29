bafbda2b55abde8def2a307246bc4022be7b6448
Takuyawe <takuslugger200@gmail.com>
2024-03-29 19:06:27 +0900

    feat: theme color config

--- src/lib/veautify/index.ts
+++ src/lib/veautify/index.ts
@@ -4,7 +4,6 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { aliases, fa } from 'vuetify/iconsets/fa';
 import { VCalendar } from 'vuetify/labs/VCalendar';
-import { VIcon } from 'vuetify/components';
 
 /// Calendarコンポーネントを追加しただけなので、気にしないで！
 const customComponents = {
