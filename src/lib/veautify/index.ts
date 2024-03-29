96c9fbf42dd3c601120807563db044ae3d0caf41
Takuyawe <takuslugger200@gmail.com>
2024-03-29 18:57:21 +0900

    fix: change configuration

--- src/lib/veautify/index.ts
+++ src/lib/veautify/index.ts
@@ -4,12 +4,16 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { aliases, fa } from 'vuetify/iconsets/fa';
 import { VCalendar } from 'vuetify/labs/VCalendar';
+import { VIcon } from 'vuetify/components';
+
+/// Calendarコンポーネントを追加しただけなので、気にしないで！
+const customComponents = {
+  ...components,
+  VCalendar,
+};
 
 const veautify = createVuetify({
-  components: {
-    components,
-    VCalendar,
-  },
+  components: customComponents,
   directives,
   icons: {
     defaultSet: 'fa',
