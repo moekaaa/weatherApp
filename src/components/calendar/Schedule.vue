7822023904d5b63beeb145c5f7dcc56731731eff
moekaaa <ramenlady22@gmail.com>
2024-04-01 08:12:51 +0900

    refactor: rename file

--- src/components/calendar/Schedule.vue
+++ src/components/calendar/Schedule.vue
@@ -1,30 +1,20 @@
-<script setup lang="ts">
-import { ScheduleComponent } from '@syncfusion/ej2-vue-schedule';
+<script>
+import { ScheduleComponent } from '@syncfusion/ej2-vue-schedule'
 //syncfusion/ej2-vue-schedule　↓ これで作ってみようと思っています
 //https://takumi-oda.com/blog/2020/06/24/post-1405/
 //無料でできなかったらやめる
 //勉強にならないかもしれない
-import { useDate } from 'vuetify';
 </script>
 
 <template>
+     <div class ="router">
   <RouterLink v-bind:to="{ name: 'AppTop' }"> AppTop</RouterLink>
-  <!-- Veautifyのカレンダーを利用 -->
-  <!-- スタイルはカスタマイズできそう -->
-  <!-- 勉強になりそうだったら、ej2-vue-schedule使ってもいいかも？ -->
-  <!-- ただ、 ej2-vue-scheduleというライブラリを使ってる人が少ないから、そこまでしっかりしてるものでもない気がする -->
-  <v-row class="fill-height">
-    <v-col>
-      <v-sheet height="600">
-        <v-calendar
-          ref="calendar"
-          v-model="today"
-          :events="events"
-          color="primary"
-          type="month"></v-calendar>
-      </v-sheet>
-    </v-col>
-  </v-row>
+  > 
+  <RouterLink v-bind:to="{ name: 'HomeView' }"> HomeView</RouterLink></div>
+  
+
 </template>
 
-<style></style>
+<style>
+
+</style>
