311427155d87fdcb6e1cda484ba62ed817a32f29
moekaaa <ramenlady22@gmail.com>
2024-04-01 08:38:00 +0900

    chore: add message

---
+++ src/components/Calendar/Schedule.vue
@@ -0,0 +1,17 @@
+<script>
+import { ScheduleComponent } from '@syncfusion/ej2-vue-schedule'
+//syncfusion/ej2-vue-schedule　↓ これで作ってみようと思っています
+//https://takumi-oda.com/blog/2020/06/24/post-1405/
+//無料でできなかったらやめる
+//勉強にならないかもしれない
+</script>
+
+<template>
+    <RouterLink v-bind:to="{ name: 'AppTop' }"> AppTop</RouterLink>
+        
+
+</template>
+
+<style>
+
+</style>
