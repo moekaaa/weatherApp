31a81cdd7a7df8f88d3a65ee29eb7919811a7b3c
moekaaa <ramenlady22@gmail.com>
2024-04-04 07:46:35 +0900

    fix: trying to fix a bug of router

---
+++ src/router/router.ts
@@ -0,0 +1,12 @@
+/* import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
+import AppTop from '../views/AppTop.vue';
+import getRoutes from './router';
+
+
+const getRoutes = getRoutes();
+
+const router = createRouter({
+    history: createWebHistory('/'),
+    routes,
+  }); */
